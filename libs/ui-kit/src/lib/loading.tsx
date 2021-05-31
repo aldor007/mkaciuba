babca190f13b555034ff5a92ebbc56a88ecd4f66
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2021-05-31 13:57:02 +0200

    add ui-kit lib

---
+++ libs/ui-kit/src/lib/loading.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './loading.css';
+
+export const Loading = () => {
+    let circleCommonClasses = 'h-2.5 w-2.5 bg-current   rounded-full';
+
+    return (
+        <div className="w-full h-screen z-50 overflow-hidden flex flex-col items-center justify-center">
+        <div className="loader ease-linear rounded-full spinner bg-white border-4 border-t-4 border-blue-700	 h-12 w-12 mb-4"></div>
+        <h2 className="text-center text-xl font-semibold">Loading...</h2>
+        <p className="w-1/3 text-center">This may take a few seconds, please don't close this page.</p>
+    </div>);
+    //  <div className="flex justify-center">
+    //  <div className="loader center ease-linear rounded-full border-8 border-t-8 border-gray-200 h-64 w-64"></div>
+    //  </div>
+};
