b84c7998d8c934811252958e234b7cc4de696e21
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2022-12-15 23:25:29 +0100

    fix: fix loader

--- apps/photos-e2e/src/integration/app.spec.ts
+++ apps/photos-e2e/src/integration/app.spec.ts
@@ -15,18 +15,21 @@ describe('blog', () => {
   });
 
   it('should open post', () => {
+    cy.viewport(1600, 860)
     // open post
     cy.get("#root > div.max-w-screen-xl.mx-auto.grid.xl\\:grid-cols-2.gap-4 > div:nth-child(1) > div.absolute.text-lg.bg-gray.leading-snug.font-serif.top-1\\/3.sm\\:top-1\\/4.lg\\:top-1\\/2.z-10.h-16.min-w-full.justify-center.items-center.text-white > div > div:nth-child(1) > a > h1").click()
     // should have text
     cy.get("#root > div.w-full.h-full > div.post.w-full > div > div > div.max-w-screen-xl.mx-auto.post-text").contains(/[a-z]+/)
     // should see image caption
-    cy.get("#root > div.w-full.h-full > div.post.w-full > div.post-content.w-full.max-w-screen-xl.mx-auto.-mt-12.relative > div > div.max-w-screen-xl.mx-auto.post-text > div.flex.flex-wrap.-mx-1.overflow-hidden > div:nth-child(1").should('be.visible')
+    cy.get("#root > div.w-full.h-full > div.post.w-full > div.post-content.w-full.max-w-screen-xl.mx-auto.-mt-12.relative > div > div.max-w-screen-xl.mx-auto.post-text > div.flex.flex-wrap.-mx-1.overflow-hidden > div:nth-child(1)").should('be.visible')
     // should click image caption
-    cy.get("#root > div.w-full.h-full > div.post.w-full > div.post-content.w-full.max-w-screen-xl.mx-auto.-mt-12.relative > div > div.max-w-screen-xl.mx-auto.post-text > div.flex.flex-wrap.-mx-1.overflow-hidden > div:nth-child(1").click()
+    cy.get("#root > div.w-full.h-full > div.post.w-full > div.post-content.w-full.max-w-screen-xl.mx-auto.-mt-12.relative > div > div.max-w-screen-xl.mx-auto.post-text > div.flex.flex-wrap.-mx-1.overflow-hidden > div:nth-child(1)").invoke('show').click()
+    cy.scrollTo('bottom')
     // pwsp should have caption
     cy.get("div.pswp__caption").should('be.visible')
     // should close iamge
-    cy.get('button.pswp__button--close').click()
+    cy.scrollTo('top')
+    cy.get('button.pswp__button--close').invoke('show').click()
     // should have related posts
     cy.get("#root > div.w-full.h-full > div.post.w-full > div.post-content.w-full.max-w-screen-xl.mx-auto.-mt-12.relative > div > div.max-w-screen-xl.mx-auto.grid.xl\\:grid-cols-2.gap-4").children().should('have.length.greaterThan', 1)
   })
