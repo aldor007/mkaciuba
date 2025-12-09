59793e02f41211ea253a424cf03cd09137fb91f5
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2025-12-09 11:22:11 +0100

    fix: add global fetch mock for Apollo Client in test

--- apps/photos/src/app/app.spec.tsx
+++ apps/photos/src/app/app.spec.tsx
@@ -7,10 +7,20 @@ describe('App', () => {
   beforeEach(() => {
     // Mock window.scrollTo since jsdom doesn't implement it
     window.scrollTo = jest.fn();
+
+    // Mock global fetch for Apollo Client
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({ data: {} }),
+        text: () => Promise.resolve('{}'),
+      })
+    ) as jest.Mock;
   });
 
   afterEach(() => {
     cleanup();
+    jest.restoreAllMocks();
   });
 
   it('should render successfully', () => {
